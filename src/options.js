6abaa805e41a5c73a3698498f28e6a13394c4668
Narno <arnaud@ligny.org>
2017-10-29 02:37:48 +0200

    Use textContent instead of innerHTML.

--- src/options.js
+++ src/options.js
@@ -14,7 +14,7 @@
       let i;
       for (i = 0; i < objects.length; i++) {
         if (objects[i].dataset && objects[i].dataset.message) {
-          objects[i].innerHTML = chrome.i18n.getMessage(objects[i].dataset.message);
+          objects[i].textContent = chrome.i18n.getMessage(objects[i].dataset.message);
         }
       }
     }
