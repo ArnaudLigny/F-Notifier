0daba791b8b5a10b647f0067e30bc82a81a38fa3
Arnaud Ligny <arnaud@ligny.org>
2021-09-02 00:32:54 +0200

    Update to Gulp 4

--- src/background.js
+++ src/background.js
@@ -1,180 +1,192 @@
+/**
+ * Copyright (c) Arnaud Ligny <arnaud@ligny.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 /* global playSound */
 
-(function () {
-  'use strict';
+/**
+ * Config
+ */
 
-  /**
-   * Config
-   */
+const FETCH_URL = 'https://mbasic.facebook.com/';
+const HOME_URL = 'https://www.facebook.com/';
+const NOTIFICATIONS_URL = 'https://www.facebook.com/notifications';
 
-  const FETCH_URL = 'https://mbasic.facebook.com/';
-  const HOME_URL = 'https://www.facebook.com/';
-  const NOTIFICATIONS_URL = 'https://www.facebook.com/notifications';
+/**
+ * Main functions
+ */
 
-  /**
-   * Main functions
-   */
+// Notifications count function
+const notificationsCount = callback => {
+  const parser = new DOMParser();
+  const headers = new Headers({
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.112 Safari/535.1',
+  });
 
-  // Notifications count function
-  const notificationsCount = callback => {
-    const parser = new DOMParser();
-    const headers = new Headers({
-      'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.112 Safari/535.1'
-    });
+  window.fetch(FETCH_URL, {
+    method: 'GET',
+    headers,
+    cache: 'no-cache',
+    credentials: 'include',
+  })
+    .then(response => {
+      if (response.ok) {
+        return response.text();
+      }
 
-    window.fetch(FETCH_URL, {
-      method: 'GET',
-      headers,
-      cache: 'no-cache',
-      credentials: 'include'
+      throw new Error('Network response was not OK.');
     })
-      .then(response => {
-        if (response.ok) {
-          return response.text();
-        }
+    .then(data => {
+      let count = 0;
+      let currentBrowser = 'chrome';
+      const temporaryDom = parser.parseFromString(data, 'text/html');
 
-        throw new Error('Network response was not OK.');
-      })
-      .then(data => {
-        let count = 0;
-        let currentBrowser = 'chrome';
-        const tmpDom = parser.parseFromString(data, 'text/html');
+      // Debug
+      console.log(temporaryDom);
 
-        // Debug
-        console.log(tmpDom);
-
-        if (tmpDom.querySelector('#header > nav > a:nth-child(3)') === null) {
-          currentBrowser = 'edge';
-          if (tmpDom.querySelector('#header > div > a:nth-child(3)') === null) {
-            throw new Error('User not connected.');
-          }
+      if (temporaryDom.querySelector('#header > nav > a:nth-child(3)') === null) {
+        currentBrowser = 'edge';
+        if (temporaryDom.querySelector('#header > div > a:nth-child(3)') === null) {
+          throw new Error('User not connected.');
         }
-
-        let countNotifElem = tmpDom.querySelector('#header > nav > a:nth-child(3) > strong > span');
-        if (currentBrowser === 'edge') {
-          countNotifElem = tmpDom.querySelector('#header > div > a:nth-child(3) > strong');
-        }
-        let countReqElem = tmpDom.querySelector('#header > nav > a:nth-child(4) > span');
-        if (currentBrowser === 'edge') {
-          countReqElem = tmpDom.querySelector('#header > div > a:nth-child(4)');
-        }
-        if (countNotifElem) {
-          count += parseInt(countNotifElem.textContent.match(/(\d+)/g)[0], 10);
-        }
-        if (countReqElem && localStorage.getItem('isFriendsReq') === 'true') {
-          count += parseInt(countReqElem.textContent.match(/(\d+)/g)[0], 10);
-        }
-
-        callback(count);
-      })
-      .catch(callback);
-  };
-
-  // Update badge
-  function updateBadge() {
-    notificationsCount(count => {
-      if (count instanceof Error) {
-        render(
-          '?',
-          [190, 190, 190, 255],
-          chrome.i18n.getMessage('browserActionErrorTitle')
-        );
-      } else {
-        render(
-          count > 0 ? count.toString() : '',
-          [208, 0, 24, 255],
-          count > 1 ? chrome.i18n.getMessage('browserActionNotifTitle', count.toString()) : chrome.i18n.getMessage('browserAction01NotifTitle', count.toString())
-        );
-        // Play sound?
-        if (
-          localStorage.getItem('isSound') === 'true' &&
-          (count > parseInt(localStorage.getItem('count'), 10) ||
-            localStorage.getItem('count') === null)
-        ) {
-          playSound();
-        }
-        localStorage.setItem('count', count);
       }
-    });
-  }
 
-  // Badge renderer
-  function render(text, color, title) {
-    chrome.browserAction.setBadgeText({text});
-    chrome.browserAction.setBadgeBackgroundColor({color});
-    chrome.browserAction.setTitle({title});
-    chrome.browserAction.setIcon({path: 'icon-38.png'});
-  }
+      let countNotifElement = temporaryDom.querySelector('#header > nav > a:nth-child(3) > strong > span');
+      let countRequestElement = temporaryDom.querySelector('#header > nav > a:nth-child(4) > span');
 
-  /**
-   * Helpers
-   */
-
-  function getTabUrl() {
-    if (parseInt(localStorage.getItem('count'), 10) > 0) {
-      if (localStorage.getItem('landingPageIfNotif') === 'home') {
-        return HOME_URL;
+      if (currentBrowser === 'edge') {
+        countNotifElement = temporaryDom.querySelector('#header > div > a:nth-child(3) > strong');
+        countRequestElement = temporaryDom.querySelector('#header > div > a:nth-child(4)');
       }
-      return NOTIFICATIONS_URL;
-    }
-    if (localStorage.getItem('landingPage') === 'notifications') {
-      return NOTIFICATIONS_URL;
-    }
-    return HOME_URL;
-  }
 
-  function openFacebookHomeInTab(tab) {
-    chrome.tabs.query({
-      currentWindow: true,
-      url: HOME_URL + '*'
-    }, tabs => {
-      if (tabs && tabs.length > 0) {
-        return chrome.tabs.update(tabs[0].id, {active: true});
+      if (countNotifElement) {
+        count += Number.parseInt(countNotifElement.textContent.match(/(\d+)/g)[0], 10);
       }
-      if (tab && tab.url === 'chrome://newtab/') {
-        return chrome.tabs.update(null, {url: getTabUrl(), active: false});
+
+      if (countRequestElement && localStorage.getItem('isFriendsReq') === 'true') {
+        count += Number.parseInt(countRequestElement.textContent.match(/(\d+)/g)[0], 10);
       }
-      return chrome.tabs.create({url: getTabUrl()});
-    });
-  }
 
-  /**
-   * Events
-   */
+      callback(count);
+    })
+    .catch(callback);
+};
 
-  // Chrome alarm
-  chrome.alarms.create({delayInMinutes: 1, periodInMinutes: 1});
-  chrome.alarms.onAlarm.addListener(updateBadge);
-
-  // Browser action
-  chrome.browserAction.onClicked.addListener(tab => {
-    updateBadge();
-    openFacebookHomeInTab(tab);
-  });
-
-  // Check whether new version is installed
-  chrome.runtime.onInstalled.addListener(details => {
-    if (details.reason === 'install') {
-      chrome.runtime.openOptionsPage();
-    }
-    updateBadge();
-  });
-
-  // On message update badge
-  chrome.runtime.onMessage.addListener(updateBadge);
-
-  // Handle connection status events
-  function handleConnectionStatus(event) {
-    if (event.type === 'online') {
-      updateBadge();
-    } else if (event.type === 'offline') {
+// Update badge
+function updateBadge() {
+  notificationsCount(count => {
+    if (count instanceof Error) {
       render(
         '?',
-        [245, 159, 0, 255],
-        chrome.i18n.getMessage('browserActionErrorTitle')
+        [190, 190, 190, 255],
+        chrome.i18n.getMessage('browserActionErrorTitle'),
       );
+    } else {
+      render(
+        count > 0 ? count.toString() : '',
+        [208, 0, 24, 255],
+        count > 1 ? chrome.i18n.getMessage('browserActionNotifTitle', count.toString()) : chrome.i18n.getMessage('browserAction01NotifTitle', count.toString()),
+      );
+      // Play sound?
+      if (
+        localStorage.getItem('isSound') === 'true'
+        && (count > Number.parseInt(localStorage.getItem('count'), 10)
+        || localStorage.getItem('count') === null)
+      ) {
+        playSound();
+      }
+
+      localStorage.setItem('count', count);
     }
+  });
+}
+
+// Badge renderer
+function render(text, color, title) {
+  chrome.browserAction.setBadgeText({text});
+  chrome.browserAction.setBadgeBackgroundColor({color});
+  chrome.browserAction.setTitle({title});
+  chrome.browserAction.setIcon({path: 'icon-38.png'});
+}
+
+/**
+ * Helpers
+ */
+
+function getTabUrl() {
+  if (Number.parseInt(localStorage.getItem('count'), 10) > 0) {
+    if (localStorage.getItem('landingPageIfNotif') === 'home') {
+      return HOME_URL;
+    }
+
+    return NOTIFICATIONS_URL;
   }
-  window.addEventListener('online', handleConnectionStatus);
-  window.addEventListener('offline', handleConnectionStatus);
-})();
+
+  if (localStorage.getItem('landingPage') === 'notifications') {
+    return NOTIFICATIONS_URL;
+  }
+
+  return HOME_URL;
+}
+
+function openFacebookHomeInTab(tab) {
+  chrome.tabs.query({
+    currentWindow: true,
+    url: HOME_URL + '*',
+  }, tabs => {
+    if (tabs && tabs.length > 0) {
+      return chrome.tabs.update(tabs[0].id, {active: true});
+    }
+
+    if (tab && tab.url === 'chrome://newtab/') {
+      return chrome.tabs.update(null, {url: getTabUrl(), active: false});
+    }
+
+    return chrome.tabs.create({url: getTabUrl()});
+  });
+}
+
+/**
+ * Events
+ */
+
+// Chrome alarm
+chrome.alarms.create({delayInMinutes: 1, periodInMinutes: 1});
+chrome.alarms.onAlarm.addListener(updateBadge);
+
+// Browser action
+chrome.browserAction.onClicked.addListener(tab => {
+  updateBadge();
+  openFacebookHomeInTab(tab);
+});
+
+// Check whether new version is installed
+chrome.runtime.onInstalled.addListener(details => {
+  if (details.reason === 'install') {
+    chrome.runtime.openOptionsPage();
+  }
+
+  updateBadge();
+});
+
+// On message update badge
+chrome.runtime.onMessage.addListener(updateBadge);
+
+// Handle connection status events
+function handleConnectionStatus(event) {
+  if (event.type === 'online') {
+    updateBadge();
+  } else if (event.type === 'offline') {
+    render(
+      '?',
+      [245, 159, 0, 255],
+      chrome.i18n.getMessage('browserActionErrorTitle'),
+    );
+  }
+}
+
+window.addEventListener('online', handleConnectionStatus);
+window.addEventListener('offline', handleConnectionStatus);
--- src/options.js
+++ src/options.js
@@ -4,14 +4,14 @@
   'use strict';
 
   document.addEventListener('DOMContentLoaded', () => {
-    const inputLandingPage = document.getElementById('landingPage');
-    const inputLandingPageIfNotif = document.getElementById('landingPageIfNotif');
-    const inputIsFriendsReq = document.getElementById('isFriendsReq');
-    const inputIsSound = document.getElementById('isSound');
+    const inputLandingPage = document.querySelector('#landingPage');
+    const inputLandingPageIfNotif = document.querySelector('#landingPageIfNotif');
+    const inputIsFriendsRequest = document.querySelector('#isFriendsReq');
+    const inputIsSound = document.querySelector('#isSound');
 
     // Apply translations
     function applyTranslations() {
-      const objects = document.getElementsByTagName('*');
+      const objects = document.querySelectorAll('*');
       let i;
       for (i = 0; i < objects.length; i++) {
         if (objects[i].dataset && objects[i].dataset.message) {
@@ -19,6 +19,7 @@
         }
       }
     }
+
     applyTranslations();
 
     // Laod options
@@ -28,41 +29,46 @@
       if (localStorage.getItem('landingPage') === null) {
         inputLandingPage.value = 'home';
       }
+
       inputLandingPageIfNotif.value = localStorage.getItem('landingPageIfNotif');
       if (localStorage.getItem('landingPageIfNotif') === null) {
         inputLandingPageIfNotif.value = 'notifications';
       }
+
       // Friends requests
-      inputIsFriendsReq.checked = false;
+      inputIsFriendsRequest.checked = false;
       if (localStorage.getItem('isFriendsReq') === 'true') {
-        inputIsFriendsReq.checked = true;
+        inputIsFriendsRequest.checked = true;
       }
+
       // Sound
       inputIsSound.checked = false;
       if (localStorage.getItem('isSound') === 'true') {
         inputIsSound.checked = true;
       }
     }
+
     loadOptions();
 
     // Save options
     function saveOptions() {
       localStorage.setItem('landingPage', inputLandingPage.value);
       localStorage.setItem('landingPageIfNotif', inputLandingPageIfNotif.value);
-      localStorage.setItem('isFriendsReq', inputIsFriendsReq.checked);
+      localStorage.setItem('isFriendsReq', inputIsFriendsRequest.checked);
       localStorage.setItem('isSound', inputIsSound.checked);
       chrome.runtime.sendMessage({do: 'updatebadge'});
     }
-    document.getElementById('landingPage').addEventListener('change', () => {
+
+    document.querySelector('#landingPage').addEventListener('change', () => {
       saveOptions();
     });
-    document.getElementById('landingPageIfNotif').addEventListener('change', () => {
+    document.querySelector('#landingPageIfNotif').addEventListener('change', () => {
       saveOptions();
     });
-    document.getElementById('isFriendsReq').addEventListener('change', () => {
+    document.querySelector('#isFriendsReq').addEventListener('change', () => {
       saveOptions();
     });
-    document.getElementById('isSound').addEventListener('change', () => {
+    document.querySelector('#isSound').addEventListener('change', () => {
       saveOptions();
       if (inputIsSound.checked === true) {
         playSound();
